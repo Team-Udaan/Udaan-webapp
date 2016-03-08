704aefd05d9d40f20413c09420592ec52d16e74e
Rohit_Patel <rvprvprvp7824@gmail.com>
2016-03-09 05:17:44 +0530

    Footer and header are ready with login modal(not validation)

---
+++ Header + footer/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    
+    //The default actions needed for materialize property usage
+    $('.modal-trigger').leanModal();
+    $(".button-collapse").sideNav();
+
+
+  });
+       
