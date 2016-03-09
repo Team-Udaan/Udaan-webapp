f6a63e46fb4a78893cec522ef2b397b710dbb7a1
Rohit_Patel <Rohitpa7824@github.com>
2016-03-09 17:44:09 +0530

    Our Team Page (fonts not set yet.. dont have them yet..)

---
+++ our-team/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    
+    //The default actions needed for materialize property usage
+    $('.modal-trigger').leanModal();
+    $(".button-collapse").sideNav();
+    var car1=$("#firstcaro");
+      car1.owlCarousel({
+ 	items : 3,
+    itemsCustom : true,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,2],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+      autoPlay: 4000,
+      singleItem:true,
+      itemsDesktop: true,
+      scrollPerPage:true,
+      paginationSpeed : 1800,
+  	});
+
+    var car2=$("#seccaro");
+      car2.owlCarousel({
+ 	items : 3,
+    itemsCustom : true,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,2],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+      autoPlay: 4000,
+      singleItem:true,
+      itemsDesktop: true,
+      scrollPerPage:true,
+      paginationSpeed : 1800,
+  	});
+  });
+       
